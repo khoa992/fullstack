8b018a7cefe528a6f0e4c11340d63929cafc79ae
khoa992 <trongkhoa.nguyen@tuni.fi>
2024-12-07 01:18:32 +0200

    ws impl

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const http = require("http");
+const WebSocket = require("ws");
 const connectDB = require("./config/db.js");
 const movieRoutes = require("./routes/movieRoutes.js");
 const authRoutes = require("./routes/auth.js");
 const seedAdmin = require("./scripts/seedAdmin.js");
 
+const { initializeWebSocket } = require("./handler/wsHandler");
+
 const app = express();
 const PORT = 3005;
+const LOCAL_URL = "http://localhost";
 
+// Middleware
 app.use(express.json());
 app.use(morgan("dev"));
 
+// HTTP Server
+const server = http.createServer(app);
+
+// Routes
 app.get("/", async (req, res) => {
   res.send("Welcome to the movie collection");
 });
@@ -20,12 +30,23 @@ app.get("/", async (req, res) => {
 app.use("/movies", movieRoutes);
 app.use("/auth", authRoutes);
 
+// WebSocket Server Initialization
+const startWsServer = (server) => {
+  const wsServer = new WebSocket.Server({ server });
+  initializeWebSocket(wsServer);
+};
+
+// Database Connection and Server Start
 const startServer = async () => {
   await connectDB();
   await seedAdmin();
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+
+  // Start the combined HTTP + WebSocket server
+  server.listen(PORT, () => {
+    console.log(`Server is running on ${LOCAL_URL}:${PORT}`);
   });
+
+  startWsServer(server);
 };
 
 startServer();
---
+++ handler/wsHandler.js
@@ -0,0 +1,35 @@
+const event = {
+  CONNECTION: "connection",
+  MESSAGE: "message",
+  CLOSE: "close",
+  ERROR: "error",
+};
+
+const initializeWebSocket = (wsServer) => {
+  const clients = new Set();
+
+  wsServer.on(event.CONNECTION, (ws) => {
+    console.log("New client connected");
+    clients.add(ws);
+
+    ws.on(event.ERROR, console.error);
+
+    ws.on(event.MESSAGE, function message(data, isBinary) {
+      console.log(`Received message ${data}`);
+      clients.forEach(function each(client) {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(data, { binary: isBinary });
+        }
+      });
+    });
+
+    ws.on(event.CLOSE, () => {
+      console.log("Client disconnected");
+      clients.delete(ws);
+    });
+  });
+
+  console.log("WebSocket server initialized");
+};
+
+module.exports = { initializeWebSocket };
